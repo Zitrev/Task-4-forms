674e33f39cb37dd32948d967eea9ab470cfeae3a
Sovetnokov Oleksandr <zitrev2@gmail.com>
2024-04-05 14:58:44 +0300

    V 1.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.About = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.SqrtSumOut = new System.Windows.Forms.TextBox();
+            this.SumSqrtsOut = new System.Windows.Forms.TextBox();
+            this.ResultOut = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // About
+            // 
+            this.About.Location = new System.Drawing.Point(311, 293);
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(75, 23);
+            this.About.TabIndex = 3;
+            this.About.Text = "About";
+            this.About.UseVisualStyleBackColor = true;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(79, 27);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(100, 20);
+            this.A.TabIndex = 4;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(79, 67);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(100, 20);
+            this.B.TabIndex = 5;
+            // 
+            // SqrtSumOut
+            // 
+            this.SqrtSumOut.Location = new System.Drawing.Point(79, 183);
+            this.SqrtSumOut.Name = "SqrtSumOut";
+            this.SqrtSumOut.Size = new System.Drawing.Size(100, 20);
+            this.SqrtSumOut.TabIndex = 7;
+            // 
+            // SumSqrtsOut
+            // 
+            this.SumSqrtsOut.CausesValidation = false;
+            this.SumSqrtsOut.Location = new System.Drawing.Point(79, 143);
+            this.SumSqrtsOut.Name = "SumSqrtsOut";
+            this.SumSqrtsOut.Size = new System.Drawing.Size(100, 20);
+            this.SumSqrtsOut.TabIndex = 6;
+            // 
+            // ResultOut
+            // 
+            this.ResultOut.Location = new System.Drawing.Point(19, 237);
+            this.ResultOut.Name = "ResultOut";
+            this.ResultOut.Size = new System.Drawing.Size(184, 20);
+            this.ResultOut.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(205, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 116);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Введіть A:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Введіть B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "A^2+B^2=";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "(A+B)^2=";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(19, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(128, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 415);
+            this.ClientSize = new System.Drawing.Size(415, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ResultOut);
+            this.Controls.Add(this.SqrtSumOut);
+            this.Controls.Add(this.SumSqrtsOut);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.About);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Сумма квадратів";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button About;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.TextBox SqrtSumOut;
+        private System.Windows.Forms.TextBox SumSqrtsOut;
+        private System.Windows.Forms.TextBox ResultOut;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,20 +6,86 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Forms;
+
 
 namespace Task_4_forms
 {
+  
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
          
+        }
+
+        private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string label1 = A.Text;
+            string label2 = B.Text;
+            int a = Convert.ToInt32(label1);
+            int b = Convert.ToInt32(label2);
+            double SumSqrts = Math.Pow(a, 2) + Math.Pow(b, 2);
+            double SqrtSum = Math.Pow((a + b), 2);
+            SumSqrtsOut.Text = SumSqrts.ToString();
+            SqrtSumOut.Text = SqrtSum.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string label1 = A.Text; 
+            string label2 = B.Text; 
+
+            int a = Convert.ToInt32(label1);
+            int b = Convert.ToInt32(label2);
+            double SumSqrts = Math.Pow(a, 2) + Math.Pow(b, 2);
+            double SqrtSum = Math.Pow((a + b), 2);
+            SumSqrtsOut.Text = SumSqrts.ToString();
+            SqrtSumOut.Text = SqrtSum.ToString();
+            if (SumSqrts > SqrtSum)
+            {
+                ResultOut.Text = "Сумма квадратів більше";
+            }
+            else if (SqrtSum > SumSqrts)
+            {
+                ResultOut.Text = "Квадрат сумми більше";
+            }
+            else
+            {
+                ResultOut.Text = "Некоректне введення. Число рівне 0.";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Визначити, що більше сума квадратів, або квадрат суми \n © Совєтніков Олександр, 2024", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
